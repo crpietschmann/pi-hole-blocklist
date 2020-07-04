840e2c8a1dd1c73b08cdc5318340a0ee3f09c2e4
Chris Pietschmann <chris@build5nines.com>
2020-07-04 16:15:35 -0500

    update lists info

--- helper-scripts/combine-all.sh
+++ helper-scripts/combine-all.sh
@@ -3,5 +3,6 @@ rm blocklist-social-all.txt
 # Combine All Social Media blocklists
 for f in blocklist-social-*.txt; do
     cat $f >> blocklist-social-all.txt
+    echo ' ' >> blocklist-social-all.txt
 done
 
